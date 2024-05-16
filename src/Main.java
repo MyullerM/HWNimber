d43002325e14967332fad49ec908be7dda9e126c
MariaM <myuller.masha@gmail.com>
2024-05-16 22:46:57 +0300

    HomeWork1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,20 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println("Задача 2");
+        String s1 = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета - " + s1);
+
+        System.out.println("Задача 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "e");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName1);
 
     }
 }
